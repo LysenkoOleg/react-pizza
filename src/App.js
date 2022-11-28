cf00ec203dd59bb4e3d08b97f548e9fbc8fd0dde
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2022-11-28 15:30:26 +0300

    render pizzas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
 import "./scss/app.scss";
+import pizzas from "./assets/pizzas.json";
+
+console.log(pizzas);
 
 function App() {
   return (
@@ -20,7 +23,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              <PizzaBlock title="Мексиканская" price={500} />
+              {pizzas.map((obj) => {
+                return <PizzaBlock {...obj} />;
+              })}
             </div>
           </div>
         </div>
